9e7f2c093aaac44d47dbfec58ffd585cc08bc823
natasajevtic <nataly.jevtic.95@gmail.com>
2020-06-26 19:43:22 +0200

    Implemented method for creating file with colors

--- DAN_XL_Natasa_Jevtic/Zadatak_1/Program.cs
+++ DAN_XL_Natasa_Jevtic/Zadatak_1/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+
 
 namespace Zadatak_1
 {
+    /// <summary>
+    /// This program simulates documents printing.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// This methos writes available colors of documents to file.
+        /// </summary>       
+        static void WriteColorsToFile()
+        {
+            StreamWriter writer = new StreamWriter(@"../../Paleta.txt");
+            string[] colors = { "blue", "red", "purple", "yellow", "pink", "green", "orange", "brown" };
+            foreach (string color in colors)
+            {
+                writer.WriteLine(color);
+            }
+            writer.Close();
+        }
         static void Main(string[] args)
         {
+            //invoking method for writing colors to file
+            WriteColorsToFile();
         }
     }
 }
